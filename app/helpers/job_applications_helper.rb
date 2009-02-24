4794c18718e8c11bb979222e3505a9264162f324
Adam Bachman <adam@slsdev.net>
2009-02-24 10:37:28 -0500

    updated readme (for style)

--- app/helpers/job_applications_helper.rb
+++ app/helpers/job_applications_helper.rb
@@ -1,7 +1,7 @@
 module JobApplicationsHelper
   def fields_for_address(address, &block)
     prefix = address.new_record? ? 'new' : 'existing'
-    fields_for("job_application[#{prefix}_address_attributes]", address, &block)
+    fields_for("job_application[#{prefix}_personal_address_attributes]", address, &block)
   end
 
 #  def add_task_link(name) 
@@ -9,5 +9,4 @@ module JobApplicationsHelper
 #      page.insert_html :bottom, :tasks, :partial => 'task', :object => Task.new 
 #    end 
 #  end as one
-
 end
